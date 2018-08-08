e26ea0cbf5bfcb071a3ce9761b48f58662346b37
Robert Conley <conleyrm@gmail.com>
2018-08-08 17:58:23 -0400

    Update send.ping.from.remote.device.py

--- send.ping.from.remote.device.py
+++ send.ping.from.remote.device.py
@@ -7,22 +7,98 @@ from scapy.all import *
 import threading
 from threading import Thread
 import sys
+import time
+from socket import *
 
 
-
-#Script created 7/12/18
-#Last modified 7/25/18
 #
-#This script uses SSH to connect to a remote device.
-#It then sends ICMP ping packets from it back to your pc.
-#The Ping's payload is displayed and used to identify
-#the remote device.
+#This script fingerprints a remote device. It meta that
+#can be used to create by creating a compound 
+#signature of its Ping payload, kernel version, and ssh version.
+#
 #
 #This script must be ran with sudo-
 #$>sudo python send.ping.from.remote.device.py
 #
 #
-#
+
+
+#########Notes
+    #rawLayer = pkt[Raw].show()
+    #ls(pkt)
+    #Get and print pkt.show() Raw/Load string.
+    #rawLoad = pkt.sprintf("{Raw:%Raw.load%\n}")
+    #print rawLoad
+
+    #payload = str(payload).encode("HEX")
+    #print payload
+
+    #rawLayer = pkt.getlayer(Raw).load
+    #pkt.show()
+    #pkt[Raw].show()
+    #print str(pkt.getlayer(Raw)
+    #icmpPkt = pkt[ICMP]
+    #payload = str(payload).encode("HEX")
+
+
+
+
+
+
+##Get SSH Version
+#serverIp = "192.168.1.1"
+serverIp = "192.168.1.2"
+serverPort = 22
+
+s = socket(AF_INET, SOCK_STREAM)
+
+s.connect((serverIp, serverPort))
+s.send('Hello world')
+sshbanner = s.recv(2048)
+
+
+
+
+
+
+
+#Print "TCP/IP sniffing started..."
+def printsniffingstarted():
+    print(" ")
+    print("TCP/IP sniffing started...please wait.")
+    print(" ")
+
+
+
+
+#Print "Identifying device..."
+def printidentifyingdevice():
+    print(" ")
+    print("Identifying device...please wait.")
+    print(" ")
+    devicesshver()
+
+
+
+#Print "Sending pings..."
+def printsendingpings():
+    print(" ")
+    print("Sending pings...please wait.")
+    print(" ")
+    print(" ")
+
+
+
+
+
+
+
+#Print "Generating fps..."
+def printgeneratingfp():
+    print (" ")
+    print (" ")
+    print ("Generating ICMP Ping fingerprint...please wait.")
+    print (" ")
 
 
 
@@ -32,8 +108,9 @@ class ssh:
     client = None
 
     def __init__(self, address, username, password, timeout):
-        print("Sending pings...please wait")
-        print(" ")
+        #printsniffingstarted()
+        #printsendingpings()
+
 
         #Create a new SSH client.
         self.client = client.SSHClient()
@@ -42,10 +119,14 @@ class ssh:
         self.client.set_missing_host_key_policy(client.AutoAddPolicy())
  
         #Make the connection.
-        self.client.connect("192.168.1.1", username="root", password="thepassword", look_for_keys="False", timeout=10)
+        #self.client.connect("192.168.1.1", username="root", password="blank", look_for_keys="False", timeout=10)
+        self.client.connect("192.168.1.2", username="root", password="blank", look_for_keys="False", timeout=10)
 
 
+    #This block is used for sending pings.
     def sendCommand(self, command):
+        printsniffingstarted()
+        printsendingpings()
 
         #Check if connection is made previously
         if(self.client):
@@ -67,6 +148,36 @@ class ssh:
 
 
 
+    #This block is used for getting kernel version.
+    def sendCommand1(self, command1):
+        #Check if connection is made previously
+        self.alldata1 = ""
+        if(self.client):
+            stdin, stdout, stderr = self.client.exec_command(command1)
+            while not stdout.channel.exit_status_ready():
+                #Print stdout data when available
+                if stdout.channel.recv_ready():
+                    #Retrieve the first 1024 bytes
+                    self.alldata1 = stdout.channel.recv(1024)
+                    while stdout.channel.recv_ready():
+                        #Retrieve the next 1024 bytes
+                        self.alldata1 += stdout.channel.recv(1024)
+
+
+                    #Print as string with utf8 encoding
+                    #print(str(self.alldata1))
+
+        else:
+            print("Connection not opened.")
+
+
+
+
+#Sleep.
+def sleepdelay():
+    time.sleep(.00025)
+    sniffing()
+
 
 
 
@@ -74,7 +185,6 @@ class ssh:
 def sniffing():
     #sniff(iface='enp0s25', prn= lambda x: filter="icmp and host 192.168.1.1", store=0, count=10)
     sniff(iface='enp0s25', store=0, count=50, prn=icmpPayload)
-
 
 
 
@@ -91,10 +201,17 @@ def icmpPayload(pkt):
 
 
 
+
+
 #Get the ICMP Raw/Load layer.
 def findicmprawloadlayer(pkt):
     payload = pkt.getlayer(Raw).load
-    findicmppadding(payload)
+    payloadfp = findicmppadding(payload)
+    icmppayload(payload)
+
+
+
+
 
 
 
@@ -102,34 +219,30 @@ def findicmprawloadlayer(pkt):
 
 #Get the Raw/Load layer's fingerprint. 
 def findicmppadding(payload):
+    sleepdelay()
     for i in xrange(0, len(payload) - 2):
         if (ord(payload[i]) == ord(payload[i+1]) - 1) and (ord(payload[i]) == (ord(payload[i+2]) - 2)):
-            payloadfirstcharhex = hex(ord(payload[i]))
-            payloadsecondcharhex = hex(ord(payload[i]) + 1)
-            payloadthirdcharhex = hex(ord(payload[i]) + 2)
+            payloadfpfirstcharhex = hex(ord(payload[i]))
+            payloadfpsecondcharhex = hex(ord(payload[i]) + 1)
+            payloadfpthirdcharhex = hex(ord(payload[i]) + 2)
  
-            payloadfirstchar = ord(payload[i])
-            payloadsecondchar = ord(payload[i]) + 1
-            payloadthirdchar = ord(payload[i]) + 2
+
+            payloadfpfirstchar = ord(payload[i])
+            payloadfpsecondchar = ord(payload[i]) + 1
+            payloadfpthirdchar = ord(payload[i]) + 2
 
             print (" ")
             print (" ")
-            print ("ICMP fingerprint leading characters.")
-            print (payloadfirstcharhex), (payloadsecondcharhex), (payloadthirdcharhex)
+            print ("Ping fingerprint leading characters.")
+            print (payloadfpfirstcharhex), (payloadfpsecondcharhex), (payloadfpthirdcharhex)
             print (" ")
-
-
-
+ 
             print (" ")
+            print ("Ping fingerprint.")
+            payloadfp = (payload[i: ]).encode("HEX")
+            print payloadfp
             print (" ")
-            print ("ICMP fingerprint.")
-            payloadentire = (payload[i: ]).encode("HEX")
-            print payloadentire
-            print (" ")
-            print (" ")
-
-
-            icmppayload(payload)
+            return payloadfp
             exit()
 
 
@@ -139,12 +252,29 @@ def findicmppadding(payload):
 #Print the entire payload.
 def icmppayload(payload):
     print (" ")
-    print ("ICMP full Raw layer.")
+    print ("ICMP Raw protocol layer.")
     payload = str(payload).encode("HEX")
     print (payload)
     print (" ")
     print (" ")
-    exit()
+    printidentifyingdevice()
+
+
+
+
+
+#Display OS Kernel and SSH version of remote device.
+def devicesshver():
+    print ""
+    print ""
+    print ("OS Kernel version.")
+    print kernelver
+    #print(str(connection1.alldata1))
+    print ("SSH version.")
+    print sshbanner
+
+
+
 
 
 
@@ -158,14 +288,37 @@ s.start()
 
 
 
+
+
+
 #SSH to remote device and send Pings.
-myIp = "192.168.1.1"
+#myIp = "192.168.1.1"
+myIp = "192.168.1.2"
 sshRemoteUser = "root"
-sshRemotePwd = "thepassword"
+sshRemotePwd = "blank"
 remoteCommand = "ping -c 1 192.168.1.99"
 timeOut = 0
 connection = ssh(myIp, sshRemoteUser, sshRemotePwd, timeOut)
 connection.sendCommand(remoteCommand)
+
+
+
+
+
+
+#SSH to remote device and get kernel version.
+myIp1 = "192.168.1.1"
+sshRemoteUser1 = "root"
+sshRemotePwd1 = "blank"
+remoteCommand1 = "uname -r"
+timeOut1 = 0
+connection1 = ssh(myIp1, sshRemoteUser1, sshRemotePwd1, timeOut1)
+connection1.sendCommand1(remoteCommand1)
+kernelver = (str(connection1.alldata1))
+
+
+
+printgeneratingfp()
 
 
 
